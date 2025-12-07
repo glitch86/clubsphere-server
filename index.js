606eeb406e5a382d857761483d75d4559245f088
glitch86 <mdpolokahmed825@gmail.com>
2025-12-07 08:53:57 +0600

    initial commit with mongodb setup

---
+++ index.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.oz9hxy1.mongodb.net/?appName=Cluster0`;
+
+// mongodb client
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const db = client.db("clubSphere");
+    const clubCollection = db.collection("clubs");
+    const usersCollection = db.collection("users");
+
+
+    // load all clubs
+    app.get("/clubs", async (req, res) => {
+      const result = await clubCollection.find().toArray();
+      res.send(result);
+    });
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+run().catch(console.dir);
